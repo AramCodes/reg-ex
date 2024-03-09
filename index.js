f2f763325e9083234697d3541903a41b9622f7c1
Aram <johnnyvalcourt@yahoo.com>
2024-03-09 18:18:29 -0500

    "second part"

--- index.js
+++ index.js
@@ -104,3 +104,97 @@ let myRegex2 = /s+/g;
 let result10 = difficultSpelling.match(myRegex);
 
 console.log(result10);
+
+// character matches 0 or more times
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex); // Returns ["goooooooo"]
+gPhrase.match(goRegex); // Returns ["g"]
+oPhrase.match(goRegex); // Returns null
+
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/; // Change this line
+let result11 = chewieQuote.match(chewieRegex);
+
+console.log(result11);
+
+// greedy vs lazy matches (match can be made lazxy by placing a ? mark after optional regex part)
+let string = "titanic";
+let regexs = /t[a-z]*?i/;
+string.match(regex);
+
+let text = "<h1>Winter is coming</h1>";
+let myRegexs = /<.*>/;
+let results1 = text.match(myRegexs);
+
+console.log(results1);
+
+// a caret not withing [] sqaure brackets can be made to match a part of a string at the beginning of the string
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let resultr = calRegex.test(rickyAndCal);
+console.log(resultr);
+
+// a dollar sign can be used to match a part of the regex at the last part of the string
+let caboose = "The last car on a train is the caboose";
+let lastRegex = /caboose$/; // Change this line
+let cabooseResult = lastRegex.test(caboose);
+console.log(cabooseResult);
+
+// short hands character classes
+let quoteSample5 = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // returns letters both cases, numbers and underscores
+let alphabetRegexV2Result = quoteSample5.match(alphabetRegexV2).length;
+console.log(alphabetRegexV2Result);
+
+let quoteSample6 = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // return every thing besides letters, numbers and underscores
+let results2 = quoteSample6.match(nonAlphabetRegex).length;
+console.log(results2);
+
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\d/g; // returns all numbers
+let results3 = numString.match(numRegex);
+console.log(results3);
+
+/*
+1) If there are numbers, they must be at the end.
+2) Letters can be lowercase and uppercase.
+3) At least two characters long. Two-letter names can't have numbers.
+*/
+
+let username = "JackOfAllTrades";
+let userCheck = /^[A-Za-z]{2,}\d*$/; // Change this line
+let resultUser = userCheck.test(username);
+
+console.log(resultUser);
+
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // white space
+let resultWhite = sample.match(countWhiteSpace);
+console.log(resultWhite);
+
+// password verifier regex
+
+let password = "Your sandwich will be $5.00";
+let digitRegex = /\d/g; // returns all numbers
+let lowerCaseRegex = /[a-z]/g; // returns all lowercase letters
+let upperCaseRegex = /[A-Z]/g; // returns all uppercase letters
+let symbolRegex = /[!@#$%^&*]/g; // returns all symbols
+let passwordResults = password.match(numRegex).length >= 1;
+
+console.log(passwordResults);
+// uppercase D or W or S makes it the opposite digits, letters, spaces
+// quatity specifiers {} means the regex must have a certain number of occurances,
+// f{3,6} means the f must have at least 3 and at most 6 occurances or you can leave the 6 blank to represent
+// an unlimited number of occurances. You can also have a specific number of occurances by putting one amount
+// in between curly braces {2}
+
+let quit = "qu";
+let noquit = "qt";
+let quRegex = /q(?=u)/; // a q with an u. positive look ahead
+let qRegex = /q(?!t)/; // a q without a t. negative look ahead
+quit.match(quRegex); // Returns ["q"]
+noquit.match(qRegex); // Returns ["q"]
